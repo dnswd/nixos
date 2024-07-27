bb8fa12de662edd716d5158f2c865428bc090bab
Dennis Al Baihaqi Walangadi <dennis.walangadi@gmail.com>
2024-07-27 22:59:04 +0700

    fix indicate if macro is recording

--- pkgs/neovim/config/lualine.nix
+++ pkgs/neovim/config/lualine.nix
@@ -8,6 +8,12 @@
     theme = "palenight";
     sections = { 
       lualine_c = [ "lsp_progress" ];
+      lualine_x = [ # Show is macro recording 
+        {
+          name.__raw = /* lua */ "require('noice').api.statusline.mode.get";
+          extraConfig.cond.__raw = /* lua */ "require('noice').api.statusline.mode.has";
+        }
+      ];
     };
   };
 
