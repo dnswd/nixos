3f394b073b6212a64d78983002ba1418b7f0c063
Dennis Al Baihaqi Walangadi <dennis.walangadi@gmail.com>
2024-08-21 23:48:56 +0700

    feat: keymap to cycle tabs

--- pkgs/neovim/config/tab.nix
+++ pkgs/neovim/config/tab.nix
@@ -10,6 +10,8 @@ in{
   };
   
   keymaps = [
+    (mkKeymap "n" "<Tab>" ":BufferLineCycleNext<CR>" "Cycle to next tab")
+    (mkKeymap "n" "<S-Tab>" ":BufferLineCyclePrev<CR>" "Cycle to previous tab")
     (mkKeymap "n" "<C-w>" ":bd<CR>" "Close current tab")
   ];
 
