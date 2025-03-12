cfc705061f016c17c3ed2bc2642855163f71a346
Dennis Al Baihaqi Walangadi <dennis.walangadi@gmail.com>
2025-03-13 03:24:19 +0700

    fix(treesitter): zsh filetype bug

--- pkgs/neovim/config/treesitter/config.lua
+++ pkgs/neovim/config/treesitter/config.lua
@@ -1,5 +1,8 @@
 do
     -- register .zsh script to use bash, since TS doesn't support zsh yet
     -- context: https://github.com/nvim-treesitter/nvim-treesitter/issues/655
-    require("nvim-treesitter.parsers").filetype_to_parsername.zsh = "bash"
+    -- local ft_to_parser = require"nvim-treesitter.parsers".filetype_to_parsername
+    -- ft_to_parser.zsh = "bash"
+    -- note: nvim doesnt recognize the zsh file extension to a zsh filetype, needs mapping 
+
 end
